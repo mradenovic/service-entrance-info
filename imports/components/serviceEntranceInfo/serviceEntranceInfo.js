91eb2c6b4ca1f384eb1b23b4a024811c5a92270c
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-05-13 23:09:25 -0400

    Create serviceEntranceInfo entry point component

---
+++ imports/components/serviceEntranceInfo/serviceEntranceInfo.js
@@ -0,0 +1,29 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
+
+import template from'./serviceEntranceInfo.html';
+
+import navigation from '../navigation/navigation';
+
+
+class ServiceEntranceInfoCtrl {}
+
+const name = 'serviceEntranceInfo';
+
+// create a module
+export default angular.module(name, [
+  angularMeteor,
+  uiRouter
+]).component(name, {
+  templateUrl: `imports/components/${name}/${name}.html`,
+  controller: ServiceEntranceInfoCtrl
+})
+  .config(config);
+
+function config($locationProvider, $urlRouterProvider) {
+
+  $locationProvider.html5Mode(true);
+
+  $urlRouterProvider.otherwise('/');
+}
