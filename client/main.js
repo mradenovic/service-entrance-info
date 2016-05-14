6af71750559ed029785614c22f29e1b962b7b724
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-05-13 23:13:01 -0400

    Bootstrap serviceEntranceInfo module

--- client/main.js
+++ client/main.js
@@ -1,22 +1,15 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import sei from '../imports/components/serviceEntranceInfo/serviceEntranceInfo';
 
-import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+function onReady() {
+  angular.bootstrap(document, ['serviceEntranceInfo']);
+}
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
