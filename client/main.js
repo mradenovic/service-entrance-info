576c13e9f88f3423ba8defdd7398c69b2584d2a0
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-05-14 00:08:14 -0400

    Load bootstrap.js

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import sei from '../imports/components/serviceEntranceInfo/serviceEntranceInfo';
