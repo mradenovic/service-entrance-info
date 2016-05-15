0acb9c78bcf338079a9139efa23031445413cf9c
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-05-15 11:34:25 -0400

    Create place component
    Place component consists of place search form, place details and
    place map.

---
+++ imports/components/place/place.js
@@ -0,0 +1,67 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
+
+import template from './place.html';
+
+import { Places } from '../../api/places.js'
+
+class PlaceCtrl {
+  constructor($scope) {
+    $scope.viewModel(this);
+
+/*
+    this.autorun(() => {
+      if (GoogleMaps.loaded()) {
+        $("#address").geocomplete();
+      }
+    });
+*/
+
+    this.helpers({
+      places() {
+        const selector ={};
+
+        for (let attr in this.getReactively('place', true)) {
+          if (this.getReactively('place.'+attr) != '') {
+            selector[attr] = this.getReactively('place.'+attr);
+          } else {
+            delete selector[attr];
+          }
+        }
+
+        return Places.find(selector);
+      }
+    })
+  }
+
+  addPlace() {
+    let place = this.place;
+    // Clear form
+    this.place = null;
+  }
+
+  removePlace(place) {
+    Places.remove(place._id);
+  }
+}
+
+const name = 'place';
+
+export default angular.module(name, [
+  angularMeteor,
+  uiRouter,
+])
+  .component(name, {
+    templateUrl: 'imports/components/place/place.html',
+    controller: ['$scope', PlaceCtrl]
+})
+  .config(config);
+
+function config($stateProvider) {
+  $stateProvider
+    .state('place', {
+      url: '/',
+      template: '<place></place>'
+    });
+}
