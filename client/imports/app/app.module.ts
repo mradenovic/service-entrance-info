d76d26b6be72045ef088fe25cf94535171c5f1a3
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-11-30 14:48:16 -0500

    feat(): add AppRoutingModule
    Add routing module and set SearchComponent as the app root.

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   bootstrap: [
     AppComponent
