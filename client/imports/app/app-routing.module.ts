5ebf24d6a19ee7f5a08338b87a6a8735288e46fa
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-11-30 21:16:09 -0500

    feat(client): add PlacesComponent route

--- client/imports/app/app-routing.module.ts
+++ client/imports/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SearchComponent } from './search/search.component'
+import { PlacesComponent } from './places/places.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/search', pathMatch: 'full' },
   { path: 'search',  component: SearchComponent },
+  { path: 'places',  component: PlacesComponent },
 ];
 
 @NgModule({
