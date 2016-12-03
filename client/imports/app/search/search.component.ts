a913b01a86c8b02b5f9b8bdf6a63fb0c93d91684
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-03 17:15:33 -0500

    feat(client): add maps autocomplete to Search form

--- client/imports/app/search/search.component.ts
+++ client/imports/app/search/search.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import {MapsAPILoader} from 'angular2-google-maps/core';
 
 import template from './search.component.html'
+
+declare var google: any;
 
 @Component({
   selector: 'search',
   template: template,
 })
-export class SearchComponent  { }
+export class SearchComponent  {
+  place: any;
+
+  constructor(
+    private _loader: MapsAPILoader, private zone: NgZone) {
+  }
+
+  ngOnInit() {
+    this.autocomplete()
+  }
+
+  autocomplete() {
+    this._loader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(document.getElementById("address"), {});
+      google.maps.event.addListener(autocomplete, 'place_changed', () => {
+        this.zone.run(() => {
+          this.place = autocomplete.getPlace();
+          console.log(this.place);
+        })
+      });
+    });
+  }
+}
