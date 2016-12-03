45945b0214b2ae4aa674fbce167d5b350fef73c3
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-03 15:37:41 -0500

    feat(client): load FormsModule into AppModule

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AgmCoreModule } from 'angular2-google-maps/core';
+import { FormsModule } from '@angular/forms';
 
 import { MAPS_API_LOADER_CONFIG } from './app.credentials'
 import { AppRoutingModule } from './app-routing.module'
@@ -13,6 +14,7 @@ import { PlacesComponent } from './places/places.component'
     BrowserModule,
     AppRoutingModule,
     AgmCoreModule.forRoot(MAPS_API_LOADER_CONFIG),
+    FormsModule,
   ],
   declarations: [
     AppComponent,
