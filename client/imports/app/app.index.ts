6f0d1959c8324a89e6565dec8f2def59ae45d50a
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-04 21:21:41 -0500

    feat(client): import NavigationComponent

--- client/imports/app/app.index.ts
+++ client/imports/app/app.index.ts
@@ -1,10 +1,12 @@
 import { AppComponent } from './app/app.component';
+import { NavigationComponent } from './navigation/navigation.component'
 import { SearchComponent } from './search/search.component';
 import { PlacesComponent } from './places/places.component'
 import { PlaceComponent } from './place/place.component'
 
 export  const COMPONENTS = [
   AppComponent,
+  NavigationComponent,
   SearchComponent,
   PlacesComponent,
   PlaceComponent,
