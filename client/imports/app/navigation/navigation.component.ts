3d9c3c58e5b272399af48f50f218ce16bea5c37d
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-04 21:21:09 -0500

    feat(client): add NavigationComponent

---
+++ client/imports/app/navigation/navigation.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import template from './navigation.component.html'
+
+@Component({
+  selector: 'navigation',
+  template: template,
+})
+export class NavigationComponent { }
