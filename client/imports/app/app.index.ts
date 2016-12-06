90b581653288bc94db40f79352bd545f55a4a063
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-06 11:49:13 -0500

    feat(client): import SearchComponent into index

--- client/imports/app/app.index.ts
+++ client/imports/app/app.index.ts
@@ -1,7 +1,9 @@
 import { AppComponent } from './app/app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { SearchComponent } from './search/search.component'
 
 export  const COMPONENTS = [
   AppComponent,
   NavigationComponent,
+  SearchComponent,
 ]
