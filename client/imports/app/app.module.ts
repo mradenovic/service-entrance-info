8039f4d667e5984677bd7c3368aff83191cfe4f8
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-06 12:12:09 -0500

    feat(client): add AppRoutingModule

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -4,10 +4,12 @@ import { AgmCoreModule } from 'angular2-google-maps/core';
 
 import { MAPS_API_LOADER_CONFIG } from './app.credentials'
 import { COMPONENTS } from './app.index';
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AgmCoreModule.forRoot(MAPS_API_LOADER_CONFIG),
   ],
   declarations: [
