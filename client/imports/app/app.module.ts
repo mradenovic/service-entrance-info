082878f705a155f7111ef8b885c3654fba0de1d5
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-06 08:36:09 -0500

    feat(client): add Google Maps support

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
+import { MAPS_API_LOADER_CONFIG } from './app.credentials'
 import { COMPONENTS } from './app.index';
 
 @NgModule({
   imports: [
     BrowserModule,
+    AgmCoreModule.forRoot(MAPS_API_LOADER_CONFIG),
   ],
   declarations: [
     ...COMPONENTS,
