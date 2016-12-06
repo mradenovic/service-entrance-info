a8cdbabc0f44f5cbcd86333f025de071a7806d7a
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-06 07:29:54 -0500

    feat(client): add AppComponent

---
+++ client/imports/app/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+import template from './app.component.html';
+
+@Component({
+  selector: 'app',
+  template: template,
+})
+export class AppComponent {}
