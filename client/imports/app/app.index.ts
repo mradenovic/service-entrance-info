15dd0c9d6aed777d24f19c78e7d54b91157fdb9b
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-06 20:32:05 -0500

    feat(client): import MapComponent into index

--- client/imports/app/app.index.ts
+++ client/imports/app/app.index.ts
@@ -1,9 +1,11 @@
 import { AppComponent } from './app/app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SearchComponent } from './search/search.component'
+import { MapComponent } from './map/map.component'
 
 export  const COMPONENTS = [
   AppComponent,
   NavigationComponent,
   SearchComponent,
+  MapComponent,
 ]
