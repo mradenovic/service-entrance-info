4798ffc96e48043ed0999eaaabd95ed79a94ffc5
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-08 21:28:58 -0500

    feat(client): add place/:place_id route

--- client/imports/app/app-routing.module.ts
+++ client/imports/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SearchComponent } from './search/search.component'
+import { PlaceComponent } from './place/place.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/search', pathMatch: 'full' },
   { path: 'search',  component: SearchComponent },
+  { path: 'place/:place_id',  component: PlaceComponent },
 ];
 
 @NgModule({
