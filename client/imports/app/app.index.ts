f6e7b91b135d6076f0f4910c8ff9350c16a1b817
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-08 21:11:32 -0500

    feat(client): import PlaceComponent into index

--- client/imports/app/app.index.ts
+++ client/imports/app/app.index.ts
@@ -2,10 +2,12 @@ import { AppComponent } from './app/app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SearchComponent } from './search/search.component'
 import { MapComponent } from './map/map.component'
+import { PlaceComponent } from './place/place.component'
 
 export  const COMPONENTS = [
   AppComponent,
   NavigationComponent,
   SearchComponent,
   MapComponent,
+  PlaceComponent,
 ]
