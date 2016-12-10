30f18f0fdc30304c073b00169b5182a3e2c2c1e6
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-10 15:23:17 -0500

    feat(*): add user login support
    Add user login support, so that authorization can be implemented.

--- both/collections/places.collection.ts
+++ both/collections/places.collection.ts
@@ -3,3 +3,7 @@ import { MongoObservable } from 'meteor-rxjs';
 import { Place } from '../models/place.model'
 
 export const Places = new MongoObservable.Collection<Place>('places');
+
+Places.allow({
+  insert: () => true,
+})
--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { AgmCoreModule } from 'angular2-google-maps/core';
+import { AccountsModule } from 'angular2-meteor-accounts-ui';
 
 import { MAPS_API_LOADER_CONFIG } from './app.credentials'
 import { COMPONENTS } from './app.index';
@@ -11,6 +12,7 @@ import { AppRoutingModule } from './app-routing.module'
     BrowserModule,
     AppRoutingModule,
     AgmCoreModule.forRoot(MAPS_API_LOADER_CONFIG),
+    AccountsModule,
   ],
   declarations: [
     ...COMPONENTS,
