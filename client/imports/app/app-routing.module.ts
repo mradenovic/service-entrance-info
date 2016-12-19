23c843c3e32f8d94e8578093aa2c3b479732d675
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-19 00:21:14 -0500

    feat(client): add 'place/:place_id/edit' route

--- client/imports/app/app-routing.module.ts
+++ client/imports/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SearchComponent } from './search/search.component'
 import { PlaceComponent } from './place/place.component'
+import { PlaceEditComponent } from './place-edit/place-edit.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/search', pathMatch: 'full' },
   { path: 'search',  component: SearchComponent },
   { path: 'place/:place_id',  component: PlaceComponent },
+  { path: 'place/:place_id/edit',  component: PlaceEditComponent },
 ];
 
 @NgModule({
