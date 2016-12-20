d2f5952f25bf6201a05dd9922b8f3843fe310f3e
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-12-19 23:37:03 -0500

    feat(client): add ServiceEntranceViewComponent

---
+++ client/imports/app/service-entrance/service-entrance-view.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input,} from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ServiceEntrance } from '../../../../both/models/service-entrance.model'
+import template from './service-entrance-view.component.html'
+
+@Component({
+  selector: 'service-entrance-view',
+  template: template,
+})
+export class ServiceEntranceViewComponent {
+  @Input() place: any;
+  @Input() se: ServiceEntrance;
+  @Input() formatted_address: string;
+
+  constructor( private router: Router ) {
+
+  }
+
+  editPlace() {
+    this.router.navigate(['place', this.place.place_id, 'edit']);
+  }
+}
