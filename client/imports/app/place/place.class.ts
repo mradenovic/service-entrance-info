3ebe5546693c449e99cd5cd3041eb18a7c9d81cf
Mischa Radenovic <miroslav.radenovic@gmail.com>
2017-04-14 16:26:45 -0400

    refactor(place): extract common 'place' code

---
+++ client/imports/app/place/place.class.ts
@@ -0,0 +1,32 @@
+import { NgZone } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { MeteorObservable } from 'meteor-rxjs';
+
+import { Places } from '../../../../both/collections/places.collection'
+import { Place } from '../../../../both/models/place.model'
+import { ServiceEntrances } from '../../../../both/collections/service-entrances.collection'
+
+export class PlaceClass {
+  place: Place;
+  serviceEntrance: any;
+  placeSubscription: Subscription;
+  serviceEntranceSubscription: Subscription;
+
+  init(place_id: string) {
+    this.placeSubscription = MeteorObservable.subscribe('place', place_id).subscribe(() => {
+      MeteorObservable.autorun().subscribe(() => {
+        this.place = Places.findOne({place_id: place_id});
+      })
+    });
+    this.serviceEntranceSubscription = MeteorObservable.subscribe('service_entrance', place_id).subscribe(() => {
+      MeteorObservable.autorun().subscribe(() => {
+        this.serviceEntrance = ServiceEntrances.findOne({place_ids: place_id});
+      })
+    });
+  }
+
+  destroy() {
+    this.placeSubscription.unsubscribe();
+    this.serviceEntranceSubscription.unsubscribe();
+  }
+}
--- client/imports/app/place/place.component.ts
+++ client/imports/app/place/place.component.ts
@@ -1,47 +1,26 @@
-import { Component, NgZone, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Subscription } from 'rxjs/Subscription';
-import { MeteorObservable } from 'meteor-rxjs';
-
-import 'rxjs/add/operator/map';
 
 import template from './place.component.html'
 
-import { Places } from '../../../../both/collections/places.collection'
-import { Place } from '../../../../both/models/place.model'
-import { ServiceEntrances } from '../../../../both/collections/service-entrances.collection'
+import { PlaceClass } from './place.class';
 
 @Component({
   selector: 'place-component',
   template: template,
 })
-export class PlaceComponent  implements OnInit {
+export class PlaceComponent  extends PlaceClass implements OnInit {
   @Input() place_id: any;
-  place: Place;
-  serviceEntrance: any;
-  placeSubscription: Subscription;
-  serviceEntranceSubscription: Subscription;
 
-  constructor(private zone: NgZone) {
-
+  constructor() {
+    super();
   }
 
   ngOnInit() {
-    let place_id = this.place_id
-    this.placeSubscription = MeteorObservable.subscribe('place', place_id).subscribe(() => {
-      MeteorObservable.autorun().subscribe(() => {
-        this.place = Places.findOne({place_id: place_id});
-      })
-    });
-    this.serviceEntranceSubscription = MeteorObservable.subscribe('service_entrance', place_id).subscribe(() => {
-      MeteorObservable.autorun().subscribe(() => {
-        this.serviceEntrance = ServiceEntrances.findOne({place_ids: place_id});
-      })
-    });
+    super.init(this.place_id);
   }
 
   ngOnDestroy() {
-    this.placeSubscription.unsubscribe();
-    this.serviceEntranceSubscription.unsubscribe();
+    super.destroy();
   }
 }
