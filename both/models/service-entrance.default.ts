f93605036b5b6db0063c8d394b28093f28a10909
Mischa Radenovic <miroslav.radenovic@gmail.com>
2017-10-13 22:12:12 -0400

    feat(models): define default service entrance data

---
+++ both/models/service-entrance.default.ts
@@ -0,0 +1,16 @@
+export class DefaultInfo {
+
+  constructor() {
+    this.created_at = new Date();
+    this.access = {
+      check_in: "None",
+      level: "L",
+      has_loading_dock: false,
+      has_stairs: false,
+      has_ramp: false,
+      has_elevator: false,
+      is_outside: false,
+      long_push: 0,
+    };
+  }
+}
