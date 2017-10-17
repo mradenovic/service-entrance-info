d7a20d422ca65fb00241423a55d56129932c4d5f
Mischa Radenovic <miroslav.radenovic@gmail.com>
2017-10-16 22:05:22 -0400

    refactor(navigation): remove unused component

--- client/imports/app/navigation/navigation.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import template from './navigation.component.html'
-
-@Component({
-  selector: 'navigation',
-  template: template,
-})
-export class NavigationComponent { }
